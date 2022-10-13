e6330680dd8784d0c568d37d9567303231fd215e
bbrianh <brianhomanchon@gmail.com>
2022-10-12 22:15:08 -0400

    prepopulated database

--- app/src/main/java/com/example/checkcoutcalculator/viewmodel/MenuViewModel.java
+++ app/src/main/java/com/example/checkcoutcalculator/viewmodel/MenuViewModel.java
@@ -5,7 +5,6 @@ import android.app.Application;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
 
 import com.example.checkcoutcalculator.db.MenuItem;
 import com.example.checkcoutcalculator.db.MenuRepository;
