5b180fc6b1beac460d8b77fe544318e81d34cadf
bbrianh <brianhomanchon@gmail.com>
2022-10-12 21:43:52 -0400

    fixed small bugs

--- app/src/main/java/com/example/checkcoutcalculator/db/ItemDao.java
+++ app/src/main/java/com/example/checkcoutcalculator/db/ItemDao.java
@@ -7,8 +7,6 @@ import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 import androidx.room.Update;
 
-import com.example.checkcoutcalculator.entities.Menu;
-
 import java.util.List;
 
 @Dao
--- app/src/main/java/com/example/checkcoutcalculator/viewmodel/MenuViewModel.java
+++ app/src/main/java/com/example/checkcoutcalculator/viewmodel/MenuViewModel.java
@@ -9,6 +9,7 @@ import androidx.lifecycle.ViewModel;
 import com.example.checkcoutcalculator.db.MenuItem;
 import com.example.checkcoutcalculator.db.MenuRepository;
 
+import java.io.Closeable;
 import java.util.List;
 
 public class MenuViewModel extends ViewModel {
@@ -17,6 +18,7 @@ public class MenuViewModel extends ViewModel {
     private final MutableLiveData<List<MenuItem>> mMenu;
 
     public MenuViewModel(Application application) {
+        super(application);
         menuRepository = new MenuRepository(application);
         mMenu = new MutableLiveData<>();
         mMenu.setValue(menuRepository.getAllItems());
